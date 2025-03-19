c7237366930de5acab0a4303b8c7b677548108e0
Madhav Arya <maarya201@gmail.com>
2025-03-19 23:17:20 +0530

    changed background color to Light Gray

--- src/Calculator.java
+++ src/Calculator.java
@@ -65,7 +65,7 @@ public class Calculator implements ActionListener {
         panel = new JPanel();
         panel.setBounds(50, 100, 300, 300);
         panel.setLayout(new GridLayout(4, 4, 10, 10));
-        panel.setBackground(Color.cyan);
+        panel.setBackground(Color.lightGray);
 
         panel.add(numberButtons[1]);
         panel.add(numberButtons[2]);
